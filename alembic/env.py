1b6b03e3ba1d3a87890d5a128ceda4baa369e0d4
Tomas Montilla <montilla.tom1@gmail.com>
2024-07-16 12:55:07 -0300

    refactor: move config.py file from config to core directory and delete config directory

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.model.models import Base
-from app.config.config import settings
+from app.core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
