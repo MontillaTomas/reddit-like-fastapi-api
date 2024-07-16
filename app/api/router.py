a2f2b018a8315ade999d00cec7d22b6652262a90
Tomas Montilla <montilla.tom1@gmail.com>
2024-07-16 12:43:47 -0300

    Add user_router to app_router

--- app/api/router.py
+++ app/api/router.py
@@ -5,5 +5,8 @@ This module creates an APIRouter instance that includes other routers.
 """
 
 from fastapi import APIRouter
+from app.api.v1.user import user_router
 
 router = APIRouter()
+
+router.include_router(user_router)
