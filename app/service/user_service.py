01b7f2d4d057141c231557f327f7b98f6af920a3
Tomas Montilla <montilla.tom1@gmail.com>
2024-07-19 11:55:23 -0300

    feat: add user_service methods for updating username or password

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -5,9 +5,9 @@ related to user entities.
 
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
-from app.schema.user import UserCreate, UserPublic
+from app.schema.user import UserCreate, UserPublic, UserUpdateUsername, UserUpdatePassword
 from app.crud.crud_user import CRUDUser
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password
 
 
 class UserService:
@@ -67,3 +67,75 @@ class UserService:
                                 detail=f"User with id={user_id} was not found")
 
         return user
+
+    def update_username(self, user_id: int, new_username: UserUpdateUsername) -> UserPublic:
+        """
+        Updates the username of a user.
+
+        Args:
+            user_id (int): ID of the user to update.
+            new_username (UserUpdateUsername): UserUpdateUsername schema instance containing the 
+            new username.
+
+        Returns:
+            UserPublic: Updated User entity object.
+
+        Raises:
+            HTTPException: If the user with the given ID is not found or the new username is
+            already registered or the new username is the same as the old username.
+        """
+        user = self.crud.get_by_id(user_id)
+
+        if user is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"User with id={user_id} was not found")
+
+        if user.username == new_username.username:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="New username is the same as the old username"
+            )
+
+        if self.crud.get_by_username(new_username.username):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail="Username already registered"
+            )
+
+        user.username = new_username.username
+        return self.crud.update_user(user)
+
+    def update_password(self, user_id: int, password_schema: UserUpdatePassword) -> UserPublic:
+        """
+        Updates the password of a user.
+
+        Args:
+            user_id (int): ID of the user to update.
+            password_schema (UserUpdatePassword): UserUpdatePassword schema instance containing the 
+            old and new passwords.
+
+        Returns:
+            UserPublic: Updated User entity object.
+
+        Raises:
+            HTTPException: If the user with the given ID is not found or the old password is 
+            incorrect or the new password is the same as the old password
+        """
+        user = self.crud.get_by_id(user_id)
+        if user is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"User with id={user_id} was not found")
+
+        if not verify_password(password_schema.old_password, user.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Old password is incorrect"
+            )
+
+        if password_schema.old_password == password_schema.new_password:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="New password cannot be the same as the old password"
+            )
+
+        user.password = hash_password(password_schema.new_password)
+        return self.crud.update_user(user)
