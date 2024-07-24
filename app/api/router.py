109526123823fc8f0328ffdefff8271db3a06919
Tomas Montilla <montilla.tom1@gmail.com>
2024-07-24 12:39:24 -0300

    feat: add auth_router to app_router

--- app/api/router.py
+++ app/api/router.py
@@ -6,7 +6,9 @@ This module creates an APIRouter instance that includes other routers.
 
 from fastapi import APIRouter
 from app.api.v1.user import user_router
+from app.api.v1.auth import auth_router
 
 router = APIRouter()
 
 router.include_router(user_router)
+router.include_router(auth_router)
